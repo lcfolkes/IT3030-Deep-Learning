1eda23016b4b276e095c35b21d1f9225a278d921
lcfolkes <lars.folkestad@gmail.com>
2020-03-13 11:56:20 +0100

    changed from sequential to functional model in encoder

--- Assignment2/Encoder.py
+++ Assignment2/Encoder.py
@@ -1,11 +1,13 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
 
 class Encoder:
-
 	def __init__(self, data, size_latent_vector):
-		self.model = self.__build_model(data, size_latent_vector)
+		self.input_shape = self.__modify_input_shape((data.shape[1:]))
+		self.encoder = self.__build_model(size_latent_vector)
+		self.data = data
 
 	# check add single channel to input_shape
 	def __modify_input_shape(self, input):
@@ -13,16 +15,16 @@ class Encoder:
 			return input + (1,)
 		return input
 
-	def __build_model(self, data, size_latent_vector):
-		input_shape = self.__modify_input_shape((data.shape[1:]))
-		#Encode
-		self.model = Sequential()
-		self.model.add(Conv2D(32, kernel_size=(3, 3),activation='relu', input_shape=input_shape))
-		self.model.add(Conv2D(64, (3, 3), activation='relu'))
-		self.model.add(MaxPooling2D(pool_size=(2, 2)))
-		self.model.add(Dropout(0.25))
-		self.model.add(Flatten())
-		self.model.add(Dense(128, activation='relu'))
-		self.model.add(Dropout(0.5))
-		self.model.add(Dense(size_latent_vector, activation='relu'))
-		return self.model
+	def __build_model(self, size_latent_vector):
+		# Encode
+		visible = Input(shape=self.input_shape)
+		conv1 = Conv2D(32, kernel_size=(3, 3), activation='relu')(visible)
+		conv2 = Conv2D(64, (3, 3), activation='relu')(conv1)
+		pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
+		dropout1 = Dropout(0.25)(pool1)
+		flat = Flatten()(dropout1)
+		hidden1 = Dense(128, activation='relu')(flat)
+		dropout2 = Dropout(0.5)(hidden1)
+		output = Dense(size_latent_vector, activation='relu')(dropout2)
+		model = Model(inputs=visible, outputs=output)
+		return model
