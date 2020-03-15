6a850051588ed1c76dab4e62fc7d9e81d8ed196e
lcfolkes <lars.folkestad@gmail.com>
2020-03-15 17:21:07 +0100

    more work on autoencoder + decoder

--- Assignment2/Autoencoder.py
+++ Assignment2/Autoencoder.py
@@ -3,43 +3,32 @@ import tensorflow as tf
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D, UpSampling2D, Reshape, Conv2DTranspose
 
+from Assignment2 import Preprocessing
+from Assignment2.Decoder import Decoder
+
 
 class Autoencoder:
 
-	def __init__(self, encoder, learning_rate=0.01, loss_function='binary_crossentropy', optimizer='adadelta', epochs='500'):
-		self.encoder = encoder.encoder
-		self.decoded = self.__decode()
-		input_layer = self.encoder.layers[0]
-		self.autoencoder = Model(input_layer.output,self.decoded)
-		self.autoencoder.summary()
-		#self.autoencoder.compile(optimizer=optimizer, loss = loss_function)
+	def __init__(self, encoder, learning_rate=0.01, loss_function='binary_crossentropy', optimizer='adadelta', epochs=50,
+				 encoded_layer=None):
+		self.e = encoder.encoder
+		self.e.summary()
 
-		#x_train = encoder.data.d1_x
-		#self.autoencoder.fit(x_train, x_train, epochs=epochs, batch_size=256, shuffle=True,
+		self.d = Decoder(self.e).decoder
+		self.d.summary()
+
+		enc_input_layer = self.e.get_input_at(0)
+		enc_output_layer = self.e.get_output_at(-1)
+		self.a = Model(enc_input_layer, self.d(enc_output_layer))
+		self.a.summary()
+		#self.autoencoder.summary()
+		#self.a.compile(optimizer=optimizer, loss = loss_function)
+
+		#x_train = np.expand_dims(encoder.data, axis=len(encoder.data.shape))
+		#print(x_train.shape)
+		#self.autoencoder.fit(x_train, x_train, epochs=epochs, batch_size=128, shuffle=True,
 		#					 validation_data=(x_train, x_train))
 		pass
-
-	def __get_flat_conv_shape(self):
-		for l in self.encoder.layers:
-			if(len(l.input_shape) > len(l.output_shape)):
-				return l.output_shape[1], l.input_shape[1:]
-
-	def __decode(self):
-	 	# Decode
-		#x = Dropout(0.5)(self.encoded)
-		encoded = self.encoder.layers[-1]
-		x = Dense(128, activation='relu')(encoded.output)
-		flat_dim, conv_shape = self.__get_flat_conv_shape()
-		x = Dense(flat_dim, activation='relu')(x)
-		x = Reshape(conv_shape)(x)
-		x = Dropout(0.25)(x)
-		#x = UpSampling2D((2, 2))(x)
-		x = Conv2DTranspose(32, kernel_size=(3, 3), activation='relu')(x)
-		#x = UpSampling2D((2, 2))(x)
-		x = Conv2DTranspose(64, kernel_size=(3, 3), activation='relu')(x)
-		decoded = Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x)
-		return decoded
-
 
 
 
--- Assignment2/Encoder.py
+++ Assignment2/Encoder.py
@@ -2,31 +2,24 @@ import numpy as np
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D, UpSampling2D, Reshape, Conv2DTranspose
 
+# check add single channel to input_shape
+def modify_input_shape(input_shape):
+	if len(input_shape) == 2:
+		return input_shape + (1,)
+	return input_shape
 
 class Encoder:
 	def __init__(self, dataset, size_latent_vector):
-		data = dataset.d1_x
-		self.input_shape = self.__modify_input_shape((data.shape[1:]))
+		self.data = dataset.d1_x
+		self.input_shape = modify_input_shape((self.data.shape[1:]))
 		self.encoder = self.__encode(size_latent_vector)
-
-	# check add single channel to input_shape
-	def __modify_input_shape(self, input):
-		if (len(input) == 2):
-			return input + (1,)
-		return input
-
-	def __get_conv_shape(self, encoder):
-		for l in encoder.layers:
-			if len(l.input_shape) > len(l.output_shape):
-				print(l.input_shape)
-				return l.input_shape
 
 	def __encode(self, size_latent_vector):
 		# Encode
 		input = Input(shape=self.input_shape)
-		x = Conv2D(32, kernel_size=(3, 3), activation='relu')(input)
+		x = Conv2D(64, kernel_size=(3, 3), activation='relu')(input)
 		#x = MaxPooling2D(pool_size=(2, 2))(x)
-		x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+		x = Conv2D(32, kernel_size=(3, 3), activation='relu')(x)
 		#x = MaxPooling2D(pool_size=(2, 2))(x)
 		x = Dropout(0.25)(x)
 		x = Flatten()(x)
@@ -34,6 +27,5 @@ class Encoder:
 		x = Dropout(0.5)(x)
 		encoded = Dense(size_latent_vector, activation='relu')(x)
 		encoder = Model(inputs=input, outputs=encoded)
-		print(encoder.summary())
 		return encoder
 
