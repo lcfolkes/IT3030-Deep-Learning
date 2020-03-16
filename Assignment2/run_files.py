227ea2863cf32e50b9627573af4a23ccca8e35ac
mathiasklev <mathias.d.klev@gmail.com>
2020-03-16 19:27:52 +0100

    Small fixes

--- Assignment2/run_files.py
+++ Assignment2/run_files.py
@@ -5,15 +5,15 @@ from Assignment2.Preprocessing import Data
 from Assignment2.Autoencoder import Autoencoder
 
 data = Data('mnist')
-encoder_semi_supervised = Encoder(data.d2_x_train, 64)
-autoencoder = Autoencoder(data.d1_x, encoder_semi_supervised.model)
-classifier_semi_supervised = Classifier(data.d2_x_train, data.d2_y_train, autoencoder.encoder)
+#encoder_semi_supervised = Encoder(data.d2_x_train, 64)
+#autoencoder = Autoencoder(data.d1_x, encoder_semi_supervised.model)
+#classifier_semi_supervised = Classifier(data.d2_x_train, data.d2_y_train, autoencoder.encoder)
 
 encoder_clean = Encoder(data.d2_x_train, 64)
 classifier_supervised = Classifier(data.d2_x_train, data.d2_y_train, encoder_clean.model)
 
 print("Semi_supervised: ")
-result_with_auto = calc_accuracy_classifier(classifier_semi_supervised, data.d2_x_test, data.d2_y_test)
+#result_with_auto = calc_accuracy_classifier(classifier_semi_supervised, data.d2_x_test, data.d2_y_test)
 
 print("Supervised: ")
 result_semi_supervised = calc_accuracy_classifier(classifier_supervised, data.d2_x_test, data.d2_y_test)
